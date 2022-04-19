59b0427373bca45ffc9da55248df04e75a74c88e
I347684 <a.miranda@sap.com>
2022-04-19 14:05:18 +0100

    upload photo

--- udacity-c3-frontend/src/app/feed/feed-item/feed-item.component.ts
+++ udacity-c3-frontend/src/app/feed/feed-item/feed-item.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
-import { BehaviorSubject } from 'rxjs';
+//import { AnimationController } from "@ionic/angular"
 import { FeedItem } from '../models/feed-item.model';
 import { FeedProviderService } from '../services/feed.provider.service';
 
@@ -8,13 +8,17 @@ import { FeedProviderService } from '../services/feed.provider.service';
   selector: 'app-feed-item',
   templateUrl: './feed-item.component.html',
   styleUrls: ['./feed-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
+
 export class FeedItemComponent implements OnInit {
   @Input() feedItem: FeedItem;
+  //@ViewChild("button", { read: ElementRef }) button: ElementRef
+
   //@Input() feedItem: FeedItem
   constructor(
-    private feed: FeedProviderService
+    private feed: FeedProviderService,
+    //private animationCtrl: AnimationController
   ) { }
 
   ngOnInit() {}
@@ -22,6 +26,30 @@ export class FeedItemComponent implements OnInit {
   async likePhoto(feedItem) {
 
     await this.feed.likeItem(feedItem.id);
+    //this.animateButton()
   }
 
+  // public animateButton() {
+  //   const animation = this.animationCtrl
+  //   .create()
+  //   .addElement(this.button.nativeElement)
+  //   .duration(2000)
+  //   .beforeStyles({
+  //     opacity: 0.2
+  //   })
+  //   .afterStyles({
+  //     background: 'rgba(0, 255, 0, 0.5)'
+  //   })
+  //   .afterClearStyles(['opacity'])
+  //   .keyframes([
+  //     { offset: 0, transform: 'scale(1)' },
+  //     { offset: 0.5, transform: 'scale(1.5)' },
+  //     { offset: 1, transform: 'scale(1)' }
+  //   ])
+
+  //   animation.play()
+  // }
+
 }
+
+
